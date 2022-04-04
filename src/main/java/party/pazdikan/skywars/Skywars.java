2bffb7a36e51900fa620905e41a8bd6a70a956dd
Pazdikan <pazdikan@gmail.com>
2022-04-04 22:02:18 +0200

    Added automatic config updater

--- src/main/java/party/pazdikan/skywars/Skywars.java
+++ src/main/java/party/pazdikan/skywars/Skywars.java
@@ -1,13 +1,35 @@
 package party.pazdikan.skywars;
 
+import com.tchristofferson.configupdater.ConfigUpdater;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+
 public final class Skywars extends JavaPlugin {
+    private static Skywars instance;
+
+    public static Skywars getInstance() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
+        // Updates config.yml with new values while keeping ones that already exists.
+        saveDefaultConfig();
+        File configFile = new File(getDataFolder(), "config.yml");
+
+        try {
+            ConfigUpdater.update(this, "config.yml", configFile, Collections.emptyList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        reloadConfig();
     }
 
     @Override
