3142dd2256ac8f57216b4ad07fb33f732c816127
Pazdikan <pazdikan@gmail.com>
2022-04-04 21:46:40 +0200

    Initial commit

---
+++ src/main/java/party/pazdikan/skywars/Skywars.java
@@ -0,0 +1,17 @@
+package party.pazdikan.skywars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Skywars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
